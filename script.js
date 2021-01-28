c4750e4f6ce4017a2c2d261cf415dc3790c1deee
ulan.0418@gmail.com <ulan.0418@gmail.com>
2021-01-28 22:47:32 +0600

    new UI added

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let finalLink
 
 function setLink() {
     finalLink = waLink + document.getElementById('userNum').value
-    location.replace(finalLink)
+  //  location.replace(finalLink)
+//window.location = finalLink
+     window.open(finalLink)
 }
 
 //"https://wa.me/77028989882"
