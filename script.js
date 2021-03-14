49decfb76e6e37152d541660a5726b5cf84082e0
ulan <ulan.0418@gmail.com>
2021-03-14 19:53:57 +0600

    switched to PWA

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+window.addEventListener('load', async ()=>{
+    if(navigator.serviceWorker){
+        try {
+            const reg = await navigator.serviceWorker.register("service-worker.js")
+            console.log("SW works", reg)
+        }catch (e){
+            console.log("Service worker failed")
+        }
+    }
+})
+
 let waLink = "https://wa.me/"
 let finalLink
-
+let pNumber
 function setLink() {
-    finalLink = waLink + document.getElementById('userNum').value
-  //  location.replace(finalLink)
-//window.location = finalLink
+    pNumber = document.getElementById('userNum').value
+
+    if(pNumber.charAt(0) === '+')
+        pNumber = pNumber.substring(1)
+    finalLink = waLink + pNumber
      window.open(finalLink)
 }
-
-//"https://wa.me/77028989882"
