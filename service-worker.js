9824472dfc28a003ad8cee3f7f1d9156b439dcdc
ulan <ulan.0418@gmail.com>
2021-03-22 14:09:54 +0600

    switched to Workbox

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,7 @@
-const staticCacheName = 's-app-v1'
-const assetUrls = [
-    'index.html',
-    'script.js',
-    'main.css'
-]
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
-self.addEventListener('install', async event => {
-    const cache = await caches.open(staticCacheName)
-    await cache.addAll(assetUrls)
-})
-
-self.addEventListener('activate', async event => {
-    const cacheNames = await caches.keys()
-    await Promise.all(
-        cacheNames
-            .filter(name => name !== staticCacheName)
-            .map(name => caches.delete(name))
-    )
-})
-
-self.addEventListener('fetch', event => {
-    const {request} = event
-    const url = new URL(request.url)
-    if (url.origin === location.origin) {
-        event.respondWith(cacheFirst(event.request))
-    }else {
-        event.respondWith(networkFirst(request))
-    }
-
-})
-
-async function cacheFirst(request) {
-    const cached = await caches.match(request)
-    return cached ?? await fetch(request)
-}
-
-async function networkFirst(request) {
-    const cache = await caches.open(staticCacheName)
-    try {
-        const response = await fetch(request)
-        await cache.put(request, response.clone())
-        return response
-    } catch (e) {
-        const cached = await cache.match(request)
-        return cached ?? await caches.match('/index.html')
-    }
-}
+// This will work!
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+)
